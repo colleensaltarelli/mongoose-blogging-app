61e72a63c767a752356efeb99dc365cea1002086
Colleen Saltarelli <colleen@digitallyft.com>
2017-11-18 13:56:11 -0500

    Create an API for a blogging app that has four endpoints

---
+++ config.js
@@ -0,0 +1,7 @@
+
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+                       global.DATABASE_URL ||
+                       'mongodb://localhost/mongoose-blogging-app';
+
+exports.PORT = process.env.PORT || 8080;
+
