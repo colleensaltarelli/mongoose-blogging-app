a6532d55323319567b924dcf0384a81f6833124b
Colleen Saltarelli <colleen@digitallyft.com>
2017-11-19 15:25:06 -0500

    added seed data to file

--- config.js
+++ config.js
@@ -2,6 +2,8 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        'mongodb://localhost/mongoose-blogging-app';
-
+exports.TEST_DATABASE_URL = (
+                        process.env.TEST_DATABASE_URL ||
+                        'mongodb://localhost/mongoose-blogging-app');
 exports.PORT = process.env.PORT || 8080;
 
